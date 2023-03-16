8c72d590bcb6250c979fcb0a92de43e1e92e4ac6
Nath Korat <95289122+NathKorat@users.noreply.github.com>
2023-03-16 08:12:39 +0000

    dell all

--- README.md
+++
@@ -1,3 +0,0 @@
-# testing_repo
-To test for using github. We create this repository in in purpose to learn about github.
-Hoping this will make us more easier to colaborate in coding process.
--- greeting.js
+++
@@ -1,9 +0,0 @@
-const greeting = (word) => {
-    return name_p => `${word} ${name_p}!`
-}
-
-let hello = greeting("Hello")
-let welcome = greeting("Welcome")
-
-console.log(hello("Nath Korat"))
-console.log(welcome("Nath Korat"))
